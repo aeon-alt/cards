bb1ecd135e992e3fe82027a1ebb669d27636f6ce
aeon <okinawakanzaki@gmail.com>
2026-02-04 15:55:13 +0800

    hatdogs

--- app/Button.tsx
+++ app/Button.tsx
@@ -2,9 +2,12 @@
 
 function Button(){
 
+    const click = () => console.log("hatdog");
+
     return(
-        <div>
-            <button>Click ME</button>
+        <div className="flex justify-center items-center h-screen">
+            <button onClick={click} className="border-2 bg-red-600 text-center p-1 m-1 rounded-[12px] hover:bg-blue-600 active:bg-green-200">Click ME</button>
+            <button onClick={click} className="border-2 bg-blue-600 text-center p-1 m-1 rounded-[12px] hover:bg-red-600 active:bg-green-200">Click ME</button>
         </div>
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Button from "./Button";
 export default function Home() {
   return (
      <div>
-      
+      <Button/>
      </div>
   );
 }

97be553b30add38b3e1a2d5988b7b66f2215ed13
aeon <okinawakanzaki@gmail.com>
2026-02-04 16:52:21 +0800

    new

--- app/Button.tsx
+++ app/Button.tsx
@@ -3,11 +3,23 @@
 function Button(){
 
     const click = () => console.log("hatdog");
+    const click2 = (namnam) => console.log(`${namnam} stop clicking!`);
 
     return(
         <div className="flex justify-center items-center h-screen">
-            <button onClick={click} className="border-2 bg-red-600 text-center p-1 m-1 rounded-[12px] hover:bg-blue-600 active:bg-green-200">Click ME</button>
-            <button onClick={click} className="border-2 bg-blue-600 text-center p-1 m-1 rounded-[12px] hover:bg-red-600 active:bg-green-200">Click ME</button>
+            
+            <button 
+            onClick={click} 
+            className="border-2 bg-red-600 text-center p-1 m-1 rounded-[12px] hover:bg-blue-600 active:bg-green-200">
+                Click ME
+            </button>
+
+            <button 
+            onClick={() => click2("hatdog")} 
+            className="border-2 bg-blue-600 text-center p-1 m-1 rounded-[12px] hover:bg-red-600 active:bg-green-200">
+                Click ME
+                </button>
+
         </div>
     );
 }
